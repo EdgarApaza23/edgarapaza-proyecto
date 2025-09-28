0efd01eff0b3822e9dc51db5308696b4c8ae6096
Edgar Apaza Mamani <edgar.apazam@uab.edu.bo>
2025-09-28 00:15:38 -0400

    pwa

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 import { Home } from './app/home/home';
+import { isDevMode } from '@angular/core';
+import {provideServiceWorker} from '@angular/service-worker';
 
-bootstrapApplication(Home, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App,
+  {
+    providers:[
+      provideServiceWorker('ngsw-worker.js',{enabled:!isDevMode()}),
+    ],
+  });
